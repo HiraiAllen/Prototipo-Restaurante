22ded9ef9d57a71e26b4b7c96148315ff55b0ccf
David Chamorro <daved3464@gmail.com>
2019-03-25 13:51:19 -0500

    Responsive headerbar and animation tweaks

--- scripts/menu_bar.js
+++ scripts/menu_bar.js
@@ -1,8 +1,18 @@
 const bar = document.getElementById('menu-bar');
 const resp = document.getElementById('resp');
+let sidebar_ontop= true;
 
 bar.addEventListener('click', showMenu);
 
 function showMenu(){
-    resp.classList.toggle('active');
+    
+    if (sidebar_ontop == true)   {
+        resp.style.top = "0";
+        resp.style.visibility = "visible";        
+        sidebar_ontop = false;        
+    }else{
+        resp.style.top = "-100%";
+        resp.style.visibility = "hidden";                        
+        sidebar_ontop = true;
+    }
 }
