829f2a141518157f55ed0eee2a46d346245afdc9
David Chamorro <daved3464@gmail.com>
2019-03-26 18:04:38 -0500

    typo

--- scripts/slider.js
+++ scripts/slider.js
@@ -3,11 +3,11 @@ let images = [];
 let time = 4000;
 
 //Lista de imagenes
-images[0] = '../img/slide1.jpg';
-images[1] = '../img/slide2.jpg';
-images[2] = '../img/slide3.jpg';
-images[3] = '../img/slide4.jpg';
-images[4] = '../img/slide5.jpg';
+images[0] = './img/slide1.jpg';
+images[1] = './img/slide2.jpg';
+images[2] = './img/slide3.jpg';
+images[3] = './img/slide4.jpg';
+images[4] = './img/slide5.jpg';
 
 function changeImg(){
     document.image.src = images[i];
